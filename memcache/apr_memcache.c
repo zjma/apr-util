zjma/apr-util

commit 46d083e05d2503916f59bddcb1a5a70cbd09bdb4

Fix memcache memory leak without use of pre_cleanup in reslist.
git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.3.x@683756 13f79535-47bb-0310-9956-ffa450edef68

memcache/apr_memcache.c
@@ -295,6 +295,27 @@ static apr_status_t conn_connect(apr_memcache_conn_t *conn)
     return rv;
 }
 
+static apr_status_t conn_clean(void *data)
+{
+    apr_memcache_conn_t *conn = data;
+    struct iovec vec[2];
+    apr_size_t written;
+
+    /* send a quit message to the memcached server to be nice about it. */
+    vec[0].iov_base = MC_QUIT;
+    vec[0].iov_len = MC_QUIT_LEN;
+
+    vec[1].iov_base = MC_EOL;
+    vec[1].iov_len = MC_EOL_LEN;
+    
+    /* Return values not checked, since we just want to make it go away. */
+    apr_socket_sendv(conn->sock, vec, 2, &written);
+    apr_socket_close(conn->sock);
+
+    conn->p = NULL; /* so that destructor does not destroy the pool again */
+
+    return APR_SUCCESS;
+}
 
 static apr_status_t
 mc_conn_construct(void **conn_, void *params, apr_pool_t *pool)
@@ -310,7 +331,11 @@ mc_conn_construct(void **conn_, void *params, apr_pool_t *pool)
         return rv;
     }
 
+#if APR_HAS_THREADS
+    conn = malloc(sizeof( apr_memcache_conn_t )); /* non-pool space! */
+#else
     conn = apr_palloc(np, sizeof( apr_memcache_conn_t ));
+#endif
 
     conn->p = np;
 
@@ -334,8 +359,10 @@ mc_conn_construct(void **conn_, void *params, apr_pool_t *pool)
     rv = conn_connect(conn);
     if (rv != APR_SUCCESS) {
         apr_pool_destroy(np);
+        free(conn);
     }
     else {
+        apr_pool_cleanup_register(np, conn, conn_clean, apr_pool_cleanup_null);
         *conn_ = conn;
     }
     
@@ -347,19 +374,12 @@ static apr_status_t
 mc_conn_destruct(void *conn_, void *params, apr_pool_t *pool)
 {
     apr_memcache_conn_t *conn = (apr_memcache_conn_t*)conn_;
-    struct iovec vec[2];
-    apr_size_t written;
     
-    /* send a quit message to the memcached server to be nice about it. */
-    vec[0].iov_base = MC_QUIT;
-    vec[0].iov_len = MC_QUIT_LEN;
+    if (conn->p) {
+        apr_pool_destroy(conn->p);
+    }
 
-    vec[1].iov_base = MC_EOL;
-    vec[1].iov_len = MC_EOL_LEN;
-    
-    /* Return values not checked, since we just want to make it go away. */
-    apr_socket_sendv(conn->sock, vec, 2, &written);
-    apr_socket_close(conn->sock);
+    free(conn); /* free non-pool space */
     
     return APR_SUCCESS;
 }

commit 2506e7a5497a6b04c3e74edce83f07a82297839f

* Fix a memory leak introduced in r683756 and a free call to a non malloced   pointer in the case that the platform has no threads.   Remark: This is not a backport from trunk since code in trunk works   differently. On trunk we use the possibility to run pre cleanups on pool   destruction.
Pointed out by: trawick


git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.3.x@685262 13f79535-47bb-0310-9956-ffa450edef68

memcache/apr_memcache.c
@@ -343,6 +343,9 @@ mc_conn_construct(void **conn_, void *params, apr_pool_t *pool)
 
     if (rv != APR_SUCCESS) {
         apr_pool_destroy(np);
+#if APR_HAS_THREADS
+        free(conn);
+#endif
         return rv;
     }
 
@@ -359,7 +362,9 @@ mc_conn_construct(void **conn_, void *params, apr_pool_t *pool)
     rv = conn_connect(conn);
     if (rv != APR_SUCCESS) {
         apr_pool_destroy(np);
+#if APR_HAS_THREADS
         free(conn);
+#endif
     }
     else {
         apr_pool_cleanup_register(np, conn, conn_clean, apr_pool_cleanup_null);